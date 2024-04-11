705802480265c43ff2574dca96a58c7c32b9b91a
manuel freitas <manuelfreitashd@gmail.com>
2024-04-11 17:33:25 +0100

    secung commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function open() {
+    alert("neloo")
+    //document.getElementById("menu").classList.remove("hidden");
+  }
+
+  function close() {
+    document.getElementById("menu").classList.toggle ("hidden");
+  }
